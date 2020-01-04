1381f0607c9471fd831d8bc8d9fcf28c8b19d218
Adarsh Pawar <adarsh.pawar@bcone.com>
2020-01-04 11:25:57 +0530

    Adding Mongooses

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 //const uri = process.env.ATLAS_URI;
-mongoose.createConnection("mongodb+srv://u1:u1@cluster0-u3zl8.mongodb.net/test", { userNewParser: true, useCreateIndex: true,  useUnifiedTopology: true}
-    );
-    
+const uri = "mongodb+srv://u1:u1@cluster0-u3zl8.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
-connection.once('once', () => {
-    console.log(`MongoDB databse connection established successfully`);
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
