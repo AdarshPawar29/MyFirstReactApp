db37fcc874222bb60225c3e1f367fe4018a64192
Adarsh Pawar <adarsh.pawar@bcone.com>
2020-01-09 12:39:45 +0530

    Updates

---
+++ precache-manifest.e1a7ae76d51431ef40223da81537fb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cd43d68ab2a2537ad67feb35550a93",
+    "url": "/MyFirstReactApp/index.html"
+  },
+  {
+    "revision": "b8445f1a7c9909b04245",
+    "url": "/MyFirstReactApp/static/css/main.7c12140e.chunk.css"
+  },
+  {
+    "revision": "26b5f9edde60381c8c6d",
+    "url": "/MyFirstReactApp/static/js/2.758f879c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MyFirstReactApp/static/js/2.758f879c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8445f1a7c9909b04245",
+    "url": "/MyFirstReactApp/static/js/main.52353646.chunk.js"
+  },
+  {
+    "revision": "bc90a1656eade2d3c226",
+    "url": "/MyFirstReactApp/static/js/runtime-main.97075e4d.js"
+  }
+]);
